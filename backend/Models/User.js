8d6a3a1f617c813877900cc2a8cef761708c1ee9
ritesh1711 <ritesh098765@gmail.com>
2025-03-03 19:00:26 +0530

    upadetd student profile role

--- backend/Models/User.js
+++ backend/Models/User.js
@@ -16,6 +16,11 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true
+    },
+    role: {
+        type: String,
+        enum: ["student", "teacher", "admin"], 
+        default: "student" 
     }
 });
 
