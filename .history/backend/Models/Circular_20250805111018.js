00b0229798ce0bb6eced6af91e4648ddea76f8c1
ritesh1711 <ritesh098765@gmail.com>
2025-08-11 15:08:48 +0530

    fixed issues admin pannel

--- .history/backend/Models/Circular_20250805111018.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const circularSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  pdfPath: { type: String, required: true },       
-  createdAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Circular', circularSchema);
